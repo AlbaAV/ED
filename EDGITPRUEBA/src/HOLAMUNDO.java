14b03f2c3acd27f686ef600b768cc6b0591b3805
alumno <alumno@alumno-desktop>
2016-02-24 11:58:42 +0100

    VICTOR

--- EDGITPRUEBA/src/HOLAMUNDO.java
+++ EDGITPRUEBA/src/HOLAMUNDO.java
@@ -3,8 +3,8 @@ public class HOLAMUNDO {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HOLA BEBEEES");
-		System.out.println("Cómo hace el murloc????? GRWLGRWLGRWLGRWLGRWLGRWLGRWL");
+		System.out.println("GRWLGRWLGRWLGRWLGRWLGRWLGRWL");
+		System.out.println("Cómo hace el murloc????? HOLA BEBEEES");
 		System.out.println("Hakuna matata vive y deja vivir");
 	}
 
